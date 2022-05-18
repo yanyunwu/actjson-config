dd701e8981c1d9c7d13fc5e1fe13c2040d6dfb5c
yanyun <2480721346@qq.com>
2022-05-18 19:21:05 +0800

    初始化

---
+++ test.js
@@ -0,0 +1,13 @@
+const { ActJson } = require('.');
+
+const json = new ActJson('./config.json');
+const config = json.getConfig();
+// config.a = 2;
+// config.b = 3;
+// config.c = 4;
+
+config.a = 1;
+config.b = 2;
+config.c = 3;
+
+console.log(config);
