0ca2985862cba42405c315d16d8a640c229c4917
yanyun <2480721346@qq.com>
2022-05-18 19:44:35 +0800

    修补数组内值为null的bug

--- test.js
+++ test.js
@@ -2,13 +2,12 @@ const { ActJson } = require('.');
 
 const json = new ActJson('./config.json');
 const config = json.getConfig();
-// config.a = 2;
-// config.b = 3;
-// config.c = 4;
+config.a = 2;
+config.b = 3;
+config.c = 4;
 
 // config.a = 1;
 // config.b = 2;
 // config.c = 3;
-config.c[4] = 1;
 
 console.log(config);
